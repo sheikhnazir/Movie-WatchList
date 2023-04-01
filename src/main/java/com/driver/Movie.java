c3f873505bf523eb5490a7461464541d05242d9f
SheikhNazir <73103801+naazie@users.noreply.github.com>
2023-04-01 23:43:37 +0530

    watchlist assignment done1

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,13 +1,14 @@
 package com.driver;
 
 public class Movie {
+
     private String name;
     private int durationInMinutes;
     private double imdbRating;
 
     public Movie() {
-
     }
+
     public Movie(String name, int durationInMinutes, double imdbRating) {
         this.name = name;
         this.durationInMinutes = durationInMinutes;
